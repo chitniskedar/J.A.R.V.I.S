4b22484b9a0e8f64798145a434be424b8b53c4a7
chitniskedar <chitniskedar7@gmail.com>
2026-01-18 18:15:52 +0530

    update api url for production

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000"
+const API = "https://jarvis-production-0594.up.railway.app"
 let USER_ID = localStorage.getItem("jarvis_user") || ""
 
 /* ---------------- USER ---------------- */
